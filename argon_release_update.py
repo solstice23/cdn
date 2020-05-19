8738cd50f2d1fbc5e7582d65da7aece56203d897
solstice23 <solstice233@gmail.com>
2020-05-19 14:53:18 +0800

    Update argon_release_update.py

--- argon_release_update.py
+++ argon_release_update.py
@@ -29,6 +29,8 @@ def getArgonRelease():
         with open(filename, 'wb') as f:
             f.write(res.content)
         print("downloaded")
+    else:
+        os.environ['GITHUB_STOP_NEXT_STEPS'] = "true";
 
     with open('./argon_releases/updated_time.txt', 'w') as f:
             f.write(format(int(time.time())))
